b1270939f586f2464fdbcfd71e913e34014747b1
RadikST <desan1986@gmail.com>
2016-01-24 15:11:38 +0300

    Add form filds checks

--- PartyInvites/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -8,10 +8,19 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
-
+        [Required(ErrorMessage = "Please enter your name")]
+        //Проверка значения и вывод ошибок.
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a vallid email address")]
+        //Проверка адресса эл. почты регуляркой
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        //Проверка номера телефона
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether yoll attend")]
+        //Проверка на выбор из списка
         public bool? WillAttend { get; set; }
     }
 }
