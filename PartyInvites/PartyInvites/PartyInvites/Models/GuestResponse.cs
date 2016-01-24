61168305cddcaec10974825e6cef8e0d7a340d2a
RadikST <desan1986@gmail.com>
2016-01-24 15:05:29 +0300

    Add Thanks View

--- PartyInvites/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
