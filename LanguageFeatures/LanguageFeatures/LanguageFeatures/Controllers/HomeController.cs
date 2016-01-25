d927836b855e4a9ddfb083331b119a8355b331d2
RadSt <desan1986@gmail.com>
2016-01-25 20:29:26 +0300

    Add Result View in HomeController

--- LanguageFeatures/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -11,5 +11,21 @@ namespace LanguageFeatures.Controllers
         {
             return "Navigateto a URL to URL to show an example";
         }
+
+        public ViewResult AutoProperty()
+        {
+            //Создать новый обьект Product
+            Product myProduct=new Product();
+
+            //Установить значения свойства
+            myProduct.Name = "Kayak";
+
+            //Получить значения свойства
+            string productName = myProduct.Name;
+
+            //Сгенерировать представление
+            return View("Result", 
+                (object) String.Format("Product name: {0}", productName));
+        }
     }
 }
