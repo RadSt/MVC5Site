66cef0a54898188e917d4c5880f7048406670d50
RadSt <desan1986@gmail.com>
2016-01-25 20:13:00 +0300

    Create app for testing Add Home Controller

---
+++ LanguageFeatures/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigateto a URL to URL to show an example";
+        }
+    }
+}
