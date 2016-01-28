e82470f81355fd4113a2d70626c71beccf467e62
RadSt <desan1986@gmail.com>
2016-01-28 19:11:15 +0300

    Add extencion method to interface

--- LanguageFeatures/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using LanguageFeatures.Models;
-using System.Collections.Generic;
 
 namespace LanguageFeatures.Controllers
 {
@@ -16,60 +15,72 @@ namespace LanguageFeatures.Controllers
         public ViewResult AutoProperty()
         {
             //Создать новый обьект Product
-            Product myProduct=new Product();
+            var myProduct = new Product();
 
             //Установить значения свойства
             myProduct.Name = "Kayak";
 
             //Получить значения свойства
-            string productName = myProduct.Name;
+            var productName = myProduct.Name;
 
             //Сгенерировать представление
-            return View("Result", 
-                (object) String.Format("Product name: {0}", productName));
+            return View("Result",
+                (object) string.Format("Product name: {0}", productName));
         }
 
         public ViewResult CreateProduct()
         {
             //Создать и заполнить новый обьект Product
-            Product myProduct=new Product
+            var myProduct = new Product
             {
-                ProductID = 100, Name = "Kayak",
+                ProductID = 100,
+                Name = "Kayak",
                 Description = "A boat for one person",
-                Price  = 275M, Category = "Watersports"
+                Price = 275M,
+                Category = "Watersports"
             };
             return View("Result",
-                (object) String.Format("Category: {0}", myProduct.Category));
+                (object) string.Format("Category: {0}", myProduct.Category));
         }
 
         public ViewResult CreateCollection()
         {
             string[] stringArray = {"apple", "orrange", "plum"};
-            List<int> intList=new List<int>{10,20,30,40};
-            Dictionary<string,int> myDict=new Dictionary<string, int>
-            { {"apple",10},{"orrange",20},{"plum",30} };
+            var intList = new List<int> {10, 20, 30, 40};
+            var myDict = new Dictionary<string, int>
+            {{"apple", 10}, {"orrange", 20}, {"plum", 30}};
 
             return View("Result", (object) stringArray[1]);
         }
 
-        public ViewResult UseExtension()
+        public ViewResult UseExtensionEnumerable()
         {
-            //Создать и заполнить обьект ShoppingCart
-            ShoppingCart cart=new ShoppingCart
+            IEnumerable<Product> products = new ShoppingCart
             {
                 Products = new List<Product>
                 {
-                    new Product{Name="Kayak", Price = 275M},
-                    new Product{Name="LifeJacket", Price=48.95M},
-                    new Product{Name="Soccer ball",Price = 19.50M},
-                    new Product{Name="Corner flag",Price=34.95M}
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "LifeJacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
                 }
             };
+
+            //Создать и заполнить массив обьектов Product
+            Product[] productArray =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "LifeJacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
             // Получить общую стоимость товаров в корзине
-            decimal cartTotal = cart.TotalPrices();
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
 
             return View("Result",
-                (object) String.Format("Total: {0:c}", cartTotal));
+                (object) string.Format("CartTotal: {0:c}, ArrayTotal: {1:c}"
+                , cartTotal,arrayTotal));
         }
     }
 }
