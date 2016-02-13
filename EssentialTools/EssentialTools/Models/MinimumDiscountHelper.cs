33cc79a9a801a791dd8b61908e3ea920fb30ec1e
RadSt <desan1986@gmail.com>
2016-02-13 22:27:05 +0300

    Add MinimumDiscount Helper class

---
+++ EssentialTools/EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EssentialTools.Models
+{
+    public class MinimumDiscountHelper:IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalPram)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
