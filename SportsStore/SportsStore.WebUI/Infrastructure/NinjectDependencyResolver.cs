8914d5147c981662a49205cd4b678de0ce72d018
RadSt <desan1986@gmail.com>
2016-02-15 16:24:08 +0300

    Add Moq to IProductRepository

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -26,6 +29,14 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // Здесь размещаются привязки Ninject
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Footbal", Price = 25M},
+                new Product {Name = "SurfBoard", Price = 179M},
+                new Product {Name = "Running shoes", Price = 95M}
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
 
     }
