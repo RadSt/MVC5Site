1954aacbc2ddfd3bde34a3115c867a2651dcbf00
RadSt <desan1986@gmail.com>
2016-02-18 23:09:40 +0300

    Add Model for pagination configure project for this and write test for it

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Instrumentation;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -26,16 +27,16 @@ namespace SportsStore.UnitTests
                 new Product {ProductID = 2, Name = "P2"},
                 new Product {ProductID = 3, Name = "P3"},
                 new Product {ProductID = 4, Name = "P4"},
-                new Product {ProductID = 5, Name = "P5"},
+                new Product {ProductID = 5, Name = "P5"}
             });
             ProductController controller=new ProductController(mock.Object);
             controller.PageSize = 3;
 
             //Act
-            IEnumerable<Product> result = (IEnumerable<Product>) controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel) controller.List(2).Model;
 
             //Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length==2);
             Assert.AreEqual(prodArray[0].Name,"P4");
             Assert.AreEqual(prodArray[1].Name,"P5");
@@ -65,6 +66,33 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
             + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
             + @"<a class=""btn btn-default"" href=""Page3"">3</a>",result.ToString());
-        }   
+        }
+
+        [TestMethod]
+        public void Can_SendPagination_View_Model()
+        {
+            //Arrange
+            Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController controller=new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            ProductListViewModel result = (ProductListViewModel) controller.List(2).Model;
+
+            //Assert
+            PagingInfo pagingInfo = result.PagingInfo;
+            Assert.AreEqual(pagingInfo.CurrentPage,2);
+            Assert.AreEqual(pagingInfo.ItemsPerPage,3);
+            Assert.AreEqual(pagingInfo.TotalItems,5);
+            Assert.AreEqual(pagingInfo.TotalPages,2);
+        }
     }
 }
