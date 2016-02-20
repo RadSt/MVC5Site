3bbfb97f48d936d56bafb9f1cf03868180d31e8a
RadSt <desan1986@gmail.com>
2016-02-21 01:00:45 +0300

    Add Cart class for ShopCart

---
+++ SportsStore/SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
