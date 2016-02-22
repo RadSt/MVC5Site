1b6ea62d04cb7bb6a11da2bc90c5d81fa104349e
RadSt <desan1986@gmail.com>
2016-02-22 21:49:36 +0300

    Add Unit Tests for CartController methods

--- SportsStore/SportsStore.UnitTests/CartTests.cs
+++ SportsStore/SportsStore.UnitTests/CartTests.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -13,96 +19,155 @@ namespace SportsStore.UnitTests
         public void Can_Add_New_Lines()
         {
             //Arrange
-            Product p1=new Product{ ProductID = 1, Name = "P1" };
-            Product p2=new Product{ ProductID = 2, Name = "P2" };
-            Cart target=new Cart();
+            Product p1 = new Product {ProductID = 1, Name = "P1"};
+            Product p2 = new Product {ProductID = 2, Name = "P2"};
+            Cart target = new Cart();
 
-            
+
             //Act
-            target.AddItem(p1,1);
-            target.AddItem(p2,1);
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
             CartLine[] result = target.Lines.ToArray();
 
             //Assert
-            Assert.AreEqual(result.Length,2);
-            Assert.AreEqual(result[0].Product,p1);
-            Assert.AreEqual(result[1].Product,p2);
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Product, p1);
+            Assert.AreEqual(result[1].Product, p2);
         }
 
         [TestMethod]
         public void Add_Quantity_For_Existing_Lines()
         {
             //Arrange
-            Product p1=new Product{ProductID = 1,Name="P1"};
-            Product p2=new Product{ProductID = 2,Name = "P2"};
-            Cart target=new Cart();
+            Product p1 = new Product {ProductID = 1, Name = "P1"};
+            Product p2 = new Product {ProductID = 2, Name = "P2"};
+            Cart target = new Cart();
 
             //Act
-            target.AddItem(p1,1);
-            target.AddItem(p2,1);
-            target.AddItem(p1,10);
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
             CartLine[] result = target.Lines.ToArray();
 
             //Assert
-            Assert.AreEqual(result.Length,2);
-            Assert.AreEqual(result[0].Quantity,11);
-            Assert.AreEqual(result[1].Quantity,1);
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Quantity, 11);
+            Assert.AreEqual(result[1].Quantity, 1);
         }
 
         [TestMethod]
         public void Can_Remove_Line()
         {
             //Arrange
-            Product p1=new Product{ProductID = 1,Name="P1"};
-            Product p2=new Product{ProductID = 2,Name="P2"};
-            Product p3 = new Product { ProductID = 3, Name = "P3" };
+            Product p1 = new Product {ProductID = 1, Name = "P1"};
+            Product p2 = new Product {ProductID = 2, Name = "P2"};
+            Product p3 = new Product {ProductID = 3, Name = "P3"};
             Cart target = new Cart();
 
             //Act
-            target.AddItem(p1,1);
-            target.AddItem(p2,3);
-            target.AddItem(p3,5);
-            target.AddItem(p2,1);
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
             target.RemoweLine(p2);
 
             //Assert 
-            Assert.AreEqual(target.Lines.Where(p=>p.Product==p2).Count(),0);
-            Assert.AreEqual(target.Lines.Count(),2);
+            Assert.AreEqual(target.Lines.Where(p => p.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
         }
 
         [TestMethod]
         public void Calculate_Cart_Total()
         {
             //Arrange
-            Product p1=new Product{ProductID = 1,Name = "P1",Price = 100M};
-            Product p2=new Product{ProductID = 2,Name="P2",Price = 50M};
-            Cart target=new Cart();
+            Product p1 = new Product {ProductID = 1, Name = "P1", Price = 100M};
+            Product p2 = new Product {ProductID = 2, Name = "P2", Price = 50M};
+            Cart target = new Cart();
 
             //Act
-            target.AddItem(p1,2);
-            target.AddItem(p2,2);
+            target.AddItem(p1, 2);
+            target.AddItem(p2, 2);
             decimal result = target.ComputeTotalValue();
 
             //Assert
-            Assert.AreEqual(result,300M);
+            Assert.AreEqual(result, 300M);
         }
 
         [TestMethod]
         public void Can_Clear_Contents()
         {
             //Arrange
-            Product p1=new Product{ProductID = 1,Name = "P1",Price = 100M};
-            Product p2=new Product{ProductID = 2,Name = "P2",Price = 50M};
-            Cart target=new Cart();
+            Product p1 = new Product {ProductID = 1, Name = "P1", Price = 100M};
+            Product p2 = new Product {ProductID = 2, Name = "P2", Price = 50M};
+            Cart target = new Cart();
 
             //Act
-            target.AddItem(p1,1);
-            target.AddItem(p2,3);
-            target.AddItem(p1,5);
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p1, 5);
             target.Clear();
 
             //Assert
-            Assert.AreEqual(target.Lines.Count(),0);
+            Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+            Cart cart = new Cart();
+            CartController target = new CartController(mock.Object);
+
+
+            //Act
+            target.AddToCart(cart, 1, null);
+
+            //Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_cart_Screen()
+        {
+            //Arrange
+            Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product{ProductID = 1, Name = "P1", Category = "Apples"} 
+            });
+            Cart cart=new Cart();
+            CartController target=new CartController(mock.Object);
+
+            //Act
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            //Assert
+            Assert.AreEqual(result.RouteValues["action"],"Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contest()
+        {
+            //Arrange
+            Cart cart=new Cart();
+            CartController target=new CartController(null);
+
+            //Act
+            CartIndexViewModel result = (CartIndexViewModel) target
+                .Index(cart, "myUrl").ViewData.Model;
+
+            //Assert
+            Assert.AreEqual(result.Cart,cart);
+            Assert.AreEqual(result.ReturnUrl,"myUrl");
+
         }
     }
 }
