1255c2db8543f7c9b47a80497bb87be2d0c032d8
RadSt <desan1986@gmail.com>
2016-02-22 23:30:37 +0300

    Small refs in Checkout View for del repeating in markup

--- SportsStore/SportsStore.Domain/Entities/ShippingDetails.cs
+++ SportsStore/SportsStore.Domain/Entities/ShippingDetails.cs
@@ -9,8 +9,11 @@ namespace SportsStore.Domain.Entities
         public string Name { get; set; }
         //Обязательное для заполнения 1 поле с адресом
         [Required(ErrorMessage = "Please enter the first adress line")]
+        [Display(Name="Line 1")]
         public string Line1 { get; set; }
+        [Display(Name="Line 2")]
         public string Line2 { get; set; }
+        [Display(Name="Line 3")]
         public string Line3 { get; set; }
         //Обязательное поле для названия города
         [Required(ErrorMessage = "Please enter a city name")]
