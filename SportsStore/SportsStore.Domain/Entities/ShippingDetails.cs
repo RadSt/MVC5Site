e9c554c7cc9beef6ce39189f28973f6fc5eef450
RadSt <desan1986@gmail.com>
2016-02-22 22:37:46 +0300

    Add Model class ShipmentDetails for shipment information

---
+++ SportsStore/SportsStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        // Обязательное для заполения поле с именем
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+        //Обязательное для заполнения 1 поле с адресом
+        [Required(ErrorMessage = "Please enter the first adress line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+        //Обязательное поле для названия города
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string City { get; set; }
+        //Обязательное поле для штата
+        [Required(ErrorMessage = "Please enter a state name")]
+        public string State { get; set; }
+        public string Zip { get; set; }
+        //Обязательное поле для названия страны
+        public string Country { get; set; }
+        public bool GiftWrap { get; set; }
+
+    }
+}
