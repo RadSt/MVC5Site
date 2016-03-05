98923521f8da496bba1b7528d3e9f984c0b6327e
RadSt <desan1986@gmail.com>
2016-03-05 23:20:13 +0300

    Add Delete method realisation in Admin controller and unit test for it

--- SportsStore/SportsStore.UnitTests/AdminTests.cs
+++ SportsStore/SportsStore.UnitTests/AdminTests.cs
@@ -118,5 +118,26 @@ namespace SportsStore.UnitTests
             Assert.IsInstanceOfType(result,typeof(ViewResult));
 
         }
+
+        [TestMethod]
+        public void Can_Delete_Valid_Product()
+        {
+            //Arrange
+            Product prod=new Product{ProductID = 2,Name="Test"};
+            Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                prod,
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            AdminController target=new AdminController(mock.Object);
+            
+            //Act
+            target.Delete(prod.ProductID);
+
+            //Assert
+            mock.Verify(m=>m.DeleteProduct(prod.ProductID));
+        }
     }
 }
