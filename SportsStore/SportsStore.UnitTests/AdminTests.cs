89c3b3266313b88cbb06faf1eda62dc152bef7e9
RadSt <desan1986@gmail.com>
2016-03-05 14:50:33 +0300

    Add methods for products editing in Admin Controller and tests for it

--- SportsStore/SportsStore.UnitTests/AdminTests.cs
+++ SportsStore/SportsStore.UnitTests/AdminTests.cs
@@ -22,7 +22,7 @@ namespace SportsStore.UnitTests
             {
                 new Product {ProductID = 1, Name = "P1"},
                 new Product {ProductID = 2, Name = "P2"},
-                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 3, Name = "P3"}
             });
             AdminController target=new AdminController(mock.Object);
 
@@ -37,5 +37,50 @@ namespace SportsStore.UnitTests
             Assert.AreEqual("P3",result[2].Name);
 
         }
+
+        [TestMethod]
+        public void Can_edit_Product()
+        {
+            //Arrange
+            Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            });
+            AdminController target=new AdminController(mock.Object);
+
+            //Act
+            Product p1 = target.Edit(1).ViewData.Model as Product;
+            Product p2 = target.Edit(2).ViewData.Model as Product;
+            Product p3 = target.Edit(3).ViewData.Model as Product;
+
+            //Assert
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexisting_Product()
+        {
+            //Arrange
+            Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"}
+            });
+            AdminController target=new AdminController(mock.Object);
+
+            //Act
+            Product result= target.Edit(4).ViewData.Model as Product;
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
     }
 }
