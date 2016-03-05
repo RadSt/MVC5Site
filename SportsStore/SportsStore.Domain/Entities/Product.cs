77436d5fab0fe6e8829ba0a65604e3b7c3e076ad
RadSt <desan1986@gmail.com>
2016-03-05 16:17:37 +0300

    Add Products validation in Edit View and Products class

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -7,10 +7,16 @@ namespace SportsStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a product Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage="Please enter a positive price")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "PLease specify a category")]
         public string Category { get; set; }
     }
 }
