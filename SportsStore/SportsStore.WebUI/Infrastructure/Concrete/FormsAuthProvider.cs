3f61315f74f977fc4c9f4acc9071b255860d4a21
RadSt <desan1986@gmail.com>
2016-03-06 12:41:30 +0300

    Create authentication provider and interface for it. Bind with Ninject

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Security;
+using SportsStore.WebUI.Infrastructure.Abstract;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider:IAuthProvider
+    {
+        public bool Authenticate(string userName, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(userName, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+            }
+            return result;
+        }
+    }
+}
